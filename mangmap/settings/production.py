6d53661caa0df4f6060458dccba4a9df3e30ea77
Camille <camille@telescoop.fr>
2023-01-09 16:17:48 +0100

    fix: rollbar key + mailgun + backupdata cron

--- mangmap/settings/production.py
+++ mangmap/settings/production.py
@@ -19,6 +19,17 @@ MIDDLEWARE.append(  # noqa: F405
     "rollbar.contrib.django.middleware.RollbarNotifierMiddleware"
 )
 
+# Mailgun
+ANYMAIL = {
+    "MAILGUN_API_KEY": config.getstr("mail.api_key"),  # noqa: F405
+    "MAILGUN_API_URL": "https://api.eu.mailgun.net/v3",
+    "MAILGUN_SENDER_DOMAIN": "mail.telescoop.fr",
+}
+EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
+DEFAULT_FROM_EMAIL = "no-reply@telescoop.fr"
+SERVER_EMAIL = "no-reply@telescoop.fr"
+
+# rollbar
 ROLLBAR = {
     "access_token": config.getstr("bugs.rollbar_access_token"),
     "environment": "development" if DEBUG else "production",
@@ -36,6 +47,7 @@ class MyFileStorage(ManifestStaticFilesStorage):
 
 STATICFILES_STORAGE = "mangmap.settings.production.MyFileStorage"
 
+# BACKUP
 INSTALLED_APPS.append("telescoop_backup")  # noqa: F405
 BACKUP_ACCESS = config.getstr("backup.backup_access")  # S3 ACCESS
 BACKUP_SECRET = config.getstr("backup.backup_secret")  # S3 SECRET KEY
